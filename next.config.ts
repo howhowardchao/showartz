42a7d43f1bb7486e32b0ac790cc6dfd96b4e52cc
howhoward <howhoward.chao@gmail.com>
2025-12-17 09:59:05 +0800

    fix: allow Pinkoi CDN domains for next/image

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: '**.cdninstagram.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn01.pinkoi.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn02.pinkoi.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn03.pinkoi.com',
+      },
     ],
   },
 };
